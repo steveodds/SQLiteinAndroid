fa7a46b3dc343db342fea61e0550cf1f22be0c16
Stephen Odundo <steveremdi@gmail.com>
2019-03-22 19:59:07 +0300

    Removed unnecessary object.

--- app/src/main/java/com/example/sqliteinandroid/DBHelper.java
+++ app/src/main/java/com/example/sqliteinandroid/DBHelper.java
@@ -12,7 +12,6 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "mydb";
     public static final String DATABASE_TABLE = "student";
     public static final int DATABASE_VERSION = 1;
-    SQLiteDatabase db;
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
