af8ce80aa0ee38b4483a6d042bb046256159863c
Stephen Odundo <steveremdi@gmail.com>
2019-04-05 09:28:39 +0300

    Fixed DB code

--- app/src/main/java/com/example/sqliteinandroid/DBHelper.java
+++ app/src/main/java/com/example/sqliteinandroid/DBHelper.java
@@ -4,30 +4,63 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+import android.database.Cursor;
 
 public class DBHelper extends SQLiteOpenHelper {
-    public static final String ID = "stdid";
+    /*public static final String ID = "stdid";
     public static final String NAME = "stdname";
-    public static final String RID = "rid";
-    public static final String DATABASE_NAME = "mydb";
-    public static final String DATABASE_TABLE = "student";
+    public static final String RID = "rid";*/
+    public static final String DATABASE_NAME = "std";
+//    public static final String DATABASE_TABLE = "student";
     public static final int DATABASE_VERSION = 1;
+    private static final String TABLE_M = "mytable";
+    SQLiteDatabase db;
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        //String DB_CREATE = "CREATE DATABASE " + DATABASE_NAME;
+    public void onCreate(SQLiteDatabase db) {
+        /*String DB_CREATE = "CREATE DATABASE " + DATABASE_NAME;
         String TABLE_CREATE = "CREATE TABLE " + DATABASE_TABLE + " (" + RID + "PRIMARYKEY, AUTO_INCREMENT, " + ID + ", TEXT, " + NAME + " text)";
-        //sqLiteDatabase.execSQL(DB_CREATE);
+        sqLiteDatabase.execSQL(DB_CREATE);
         sqLiteDatabase.execSQL(TABLE_CREATE);
-        Log.d("Table", "Table created");
+        Log.d("Table", "Table created");*/
+
+        String CREATE = "CREATE TABLE " + TABLE_M +"(regno TEXT,name TEXT)"; db.execSQL(CREATE);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE);
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+//        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_M); onCreate(db);
+    }
+
+    public void writeData(String regno,String name){
+        db=getWritableDatabase();
+        db.execSQL("insert into mytable values ('"+regno+"','"+name +"')");
+    }
+
+    public void updateData(String regno,String name){
+        db=getWritableDatabase();
+        db.execSQL("update mytable set name='"+name +"'where regno='"+regno+"'");
+    }
+
+    public void deleteData(String regno){
+        db=getWritableDatabase();
+        db.execSQL("delete from mytable where regno='"+regno+"'");
+    }
+
+    public Cursor readData(String regno) {
+        Cursor c1 = null;
+        try {
+            db = this.getReadableDatabase();
+            c1 = db.rawQuery("select name from mytable where regno="+regno, null);
+        }
+        catch (Exception e) {
+            System.out.println("DATABASE ERROR " + e);
+        }
+        return c1;
     }
 }
--- app/src/main/java/com/example/sqliteinandroid/MainActivity.java
+++ app/src/main/java/com/example/sqliteinandroid/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.sqliteinandroid;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -24,6 +26,31 @@ public class MainActivity extends AppCompatActivity {
         deleteBtn = findViewById(R.id.btnDelete);
         idTxt = findViewById(R.id.txtIDVal);
         nameTxt = findViewById(R.id.txtNameVal);
-
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                dbHelper.writeData(idTxt.getText().toString(),nameTxt.getText().toString());
+            }
+        });//end
+        readBtn.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                Cursor crs= dbHelper.readData(idTxt.getText().toString());
+                if (crs.moveToFirst()){
+                    do{
+                        nameTxt.setText(crs.getString(0));
+                    }while(crs.moveToNext());
+                }
+                crs.close();
+            }
+        });//end
+        editBtn.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                dbHelper.updateData(idTxt.getText().toString(),nameTxt.getText().toString());
+            }
+        });//end
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                dbHelper.deleteData(idTxt.getText().toString());
+            }
+        });//end
     }
 }
