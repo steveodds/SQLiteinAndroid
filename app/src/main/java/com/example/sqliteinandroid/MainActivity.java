e8ba72f5391fe1a011a70b41b35de5cfe1c1a07d
Stephen Odundo <steveremdi@gmail.com>
2019-04-11 18:34:56 +0300

    Added Read/Update/Delete functionality

--- app/src/main/java/com/example/sqliteinandroid/MainActivity.java
+++ app/src/main/java/com/example/sqliteinandroid/MainActivity.java
@@ -61,32 +61,44 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 dbHelper.writeData(idTxt.getText().toString(), nameTxt.getText().toString());
             }
-        });//end
+        });
+
         readBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Cursor crs = dbHelper.readData(idTxt.getText().toString());
+                /*Cursor crs = dbHelper.readData(idTxt.getText().toString());
                 if (crs.moveToFirst()) {
                     do {
                         nameTxt.setText(crs.getString(0));
                     }
                     while (crs.moveToNext());
                 }
-                crs.close();
+                crs.close();*/
+
+                //Remote read:
+                new Read().execute();
             }
-        });//end
+        });
+
         editBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dbHelper.updateData(idTxt.getText().toString(), nameTxt.getText().toString());
+                //dbHelper.updateData(idTxt.getText().toString(), nameTxt.getText().toString());
+
+                //Server code:
+                new Update().execute();
             }
-        });//end
+        });
+
         deleteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dbHelper.deleteData(idTxt.getText().toString());
+                //dbHelper.deleteData(idTxt.getText().toString());
+
+                //Server code:
+                new Delete().execute();
             }
-        });//end
+        });
     }
 
 
@@ -171,6 +183,238 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("Fail 3", e.toString());
             }
 
+            mProgressDialog.dismiss();
+        }
+    }
+    final class Read extends AsyncTask<String, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog = new ProgressDialog(MainActivity.this);
+            mProgressDialog.setMessage("Reading record please wait..");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            mProgressDialog.setCancelable(false); mProgressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+                Uri.Builder builder = new Uri.Builder().appendQueryParameter("id", idTxt.getText().toString().trim());
+                query = builder.build().getEncodedQuery();
+                String url = "https://citrus-realignments.000webhostapp.com/student/read.php";
+                URL obj = new URL(url);
+                con = (HttpURLConnection) obj.openConnection();
+                con.setRequestMethod("POST");
+                con.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)");
+                con.setRequestProperty("Accept-Language", "UTF-8");
+                con.setDoOutput(true);
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(con.getOutputStream());
+                outputStreamWriter.write(query);
+                outputStreamWriter.flush();
+            }
+            catch (Exception e) {
+                android.util.Log.e("Fail 1", e.toString());
+            }
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String line;
+                StringBuffer sb = new StringBuffer();
+                while ((line = in.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                results = sb.toString();
+            }
+            catch (Exception e) {
+                android.util.Log.e("Fail 2", e.toString());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            try {
+                json_data = new JSONObject(results);
+                int code = (json_data.getInt("code"));
+                if (code == 1) {
+                    nameTxt.setText(json_data.getString("name"));
+                }
+                else {
+                    final AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                    alert.setTitle("Failed");
+                    alert.setMessage("Reading Student Failed");
+                    alert.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                        public void onClick(@NonNull DialogInterface dialog, int whichButton) {
+                            dialog.cancel();
+                        }
+                    });
+                    alert.show();
+                }
+            }
+            catch (Exception e) {
+                Log.e("Fail 3", e.toString());
+            }
+            mProgressDialog.dismiss();
+        }
+    }
+
+    final class Update extends AsyncTask<String, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog = new ProgressDialog(MainActivity.this);
+            mProgressDialog.setMessage("Updating record please wait..");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            mProgressDialog.setCancelable(false);
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+                Uri.Builder builder = new Uri.Builder() .appendQueryParameter("id", idTxt.getText().toString().trim()) .appendQueryParameter("name", nameTxt.getText().toString().trim());
+                query = builder.build().getEncodedQuery();
+                String url = "https://citrus-realignments.000webhostapp.com/student/update.php";
+                URL obj = new URL(url);
+                con = (HttpURLConnection) obj.openConnection();
+                con.setRequestMethod("POST");
+                con.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)");
+                con.setRequestProperty("Accept-Language", "UTF-8");
+                con.setDoOutput(true);
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(con.getOutputStream());
+                outputStreamWriter.write(query);
+                outputStreamWriter.flush();
+            }
+            catch (Exception e) {
+                android.util.Log.e("Fail 1", e.toString());
+            }
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String line;
+                StringBuffer sb = new StringBuffer();
+                while ((line = in.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                results = sb.toString();
+            }
+            catch (Exception e) {
+                android.util.Log.e("Fail 2", e.toString());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            try { json_data = new JSONObject(results);
+                int code = (json_data.getInt("code"));
+                if (code == 1) {
+                    final AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                    alert.setTitle("Success");
+                    alert.setMessage("Student Record Updated");
+                    alert.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                        public void onClick(@NonNull DialogInterface dialog, int whichButton) {
+                            dialog.cancel();
+                        }
+                    });
+                    alert.show();
+                }
+                else {
+                    final AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                    alert.setTitle("Failed");
+                    alert.setMessage("Updating Student Failed");
+                    alert.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                        public void onClick(@NonNull DialogInterface dialog, int whichButton) {
+                            dialog.cancel();
+                        }
+                    });
+                    alert.show();
+                }
+            }
+            catch (Exception e) {
+                Log.e("Fail 3", e.toString());
+            }
+            mProgressDialog.dismiss();
+        }
+    }
+
+    final class Delete extends AsyncTask<String, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog = new ProgressDialog(MainActivity.this);
+            mProgressDialog.setMessage("Deleting record please wait..");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            mProgressDialog.setCancelable(false);
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+                Uri.Builder builder = new Uri.Builder() .appendQueryParameter("id", idTxt.getText().toString().trim());
+                query = builder.build().getEncodedQuery();
+                String url = "https://citrus-realignments.000webhostapp.com/student/delete.php";
+                URL obj = new URL(url);
+                con = (HttpURLConnection) obj.openConnection();
+                con.setRequestMethod("POST");
+                con.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)");
+                con.setRequestProperty("Accept-Language", "UTF-8");
+                con.setDoOutput(true);
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(con.getOutputStream());
+                outputStreamWriter.write(query);
+                outputStreamWriter.flush();
+            }
+            catch (Exception e) {
+                android.util.Log.e("Fail 1", e.toString());
+            }
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String line;
+                StringBuffer sb = new StringBuffer();
+                while ((line = in.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                results = sb.toString();
+            }
+            catch (Exception e) {
+                android.util.Log.e("Fail 2", e.toString());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            try {
+                json_data = new JSONObject(results);
+                int code = (json_data.getInt("code"));
+                if (code == 1) {
+                    final AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                    alert.setTitle("Success");
+                    alert.setMessage("Student Record Deleted");
+                    alert.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                        public void onClick(@NonNull DialogInterface dialog, int whichButton) {
+                            dialog.cancel();
+                        }
+                    });
+                    alert.show();
+                }
+                else {
+                    final AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                    alert.setTitle("Failed");
+                    alert.setMessage("Deleting Student Failed");
+                    alert.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                        public void onClick(@NonNull DialogInterface dialog, int whichButton) {
+                            dialog.cancel();
+                        }
+                    });
+                    alert.show();
+                }
+            }
+            catch (Exception e) {
+                Log.e("Fail 3", e.toString());
+            }
             mProgressDialog.dismiss();
         }
     }
